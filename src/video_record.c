ba185ffac22832d353a6a54b1e3bfc93318b8018
Kiran Ryali <kiran.ryali@gmail.com>
2011-02-04 00:58:16 -0600

    Added library file to fix timestamp error

--- src/video_record.c
+++ src/video_record.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <sys/time.h>
+#include <linux/videodev2.h>
 #include <sys/ioctl.h>
 
 void video_record_init()
 {
 	int fd;
-	struct v4l2_capability;
-	if(ioctl(fd, VIDIOC_QUERYCAP, &v4l2_capability)==-1){
+	struct v4l2_capability cap;
+	if(ioctl(fd, VIDIOC_QUERYCAP, &cap)==-1){
 		perror("ioctl");
 	}
-    //kkkoprintf("[V_REC] This function initialize the camera device and V4L2 interface\n");
+    //printf("[V_REC] This function initialize the camera device and V4L2 interface\n");
 }
 
 void video_frame_copy()
