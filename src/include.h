17e4dbc648da12e66896be29c359f4bf8de2b3d5
kiran <kiran@kiran-ubuntu.(none)>
2011-03-25 21:17:26 -0500

    Added binding and listening on socket

---
+++ src/include.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/in.h>
+
+#define LISTEN_PORT 1336
+#define BACKLOG 10
+
+#endif
--- src/recorder_server.c
+++ src/recorder_server.c
@@ -5,6 +5,27 @@ void init_server(){
 
 	// Open up a socket
 	recorder_socket = socket( AF_INET, SOCK_STREAM, NULL );
+	if( recorder_socket == -1 ){
+		perror("socket");
+		exit(1);
+	}
+
+	// Build sock addr
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port =  htons(LISTEN_PORT);
 
 	// Bind socket
+	if( bind( recorder_socket, &addr, sizeof(addr) ) == -1) {
+		perror("bind");
+		exit(1);
+	}
+
+	// Listen on the socket for connections
+	if( listen( recorder_socket, BACKLOG ) == -1) {
+		perror("listen");
+		exit(1);
+	}
 }
