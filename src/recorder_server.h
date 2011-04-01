63a98990758c527e1ba1af91d5e7795503ebdc3f
Hughes Jonathan E <hughes11@sc-0216-linux15.ews.illinois.edu>
2011-04-01 16:38:32 -0500

    added in recorder arguments

--- src/recorder_server.h
+++ src/recorder_server.h
@@ -4,15 +4,17 @@
 #include "include.h"
 #include "video_play.h"
 
-void init_server();
-void init_control_connection();
-int recorder_socket;
+int recorder_control_socket;
+int recorder_av_socket;
 int acceptfd;
+
+void init_server();
+void init_av_connection();
+void init_control_connection();
 void establish_peer_connection();
 void listen_control_packets();
-void register_nameserver();
+void register_nameserver(char * name, char * protocol, char * control_port);
 char * nameServerMsg(char * name, char * ip, char * port, char * protocol, int * size);
-void register_nameserver(char * name);
 char *  getIP();
 
 #endif
