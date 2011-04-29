76a173c1477b14aaaf3296cc21861b648c3bd066
Kiran Ryali <kiran.ryali@gmail.com>
2011-04-28 23:41:21 -0500

    Added control thread and latencytimertask

---
+++ SmileTime/src/com/smiletime/ControlThread.java
@@ -0,0 +1,16 @@
+package com.smiletime;
+
+import java.io.InputStream;
+
+public class ControlThread extends Thread{
+	
+	InputStream in;
+	
+	public ControlThread(InputStream input){
+		in = input;
+	}
+	
+	public void run(){
+		
+	}
+}
