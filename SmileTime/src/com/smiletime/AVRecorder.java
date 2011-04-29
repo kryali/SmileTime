bc4590b09ac413ad5cd5fbf578f3220a0e80ac13
Kiran Ryali <kiran.ryali@gmail.com>
2011-04-28 23:01:10 -0500

    Added insert message to chatbox

--- SmileTime/src/com/smiletime/AVRecorder.java
+++ SmileTime/src/com/smiletime/AVRecorder.java
@@ -36,12 +36,16 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.Window;
-import android.widget.HorizontalScrollView;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -274,8 +278,16 @@ public class AVRecorder extends Activity implements SurfaceHolder.Callback {
 			}
 		});
 		
-		
+		Button but = (Button) findViewById(R.id.chatButton);
+		but.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
 
+				EditText e = (EditText) findViewById(R.id.chatInput);
+				insertMessage("[ME] " + e.getText().toString());
+			}
+		});
 	}
 	
 	public void hideTitleAndNotification(){
@@ -375,6 +387,23 @@ public class AVRecorder extends Activity implements SurfaceHolder.Callback {
 		isAudioRecording = true;
 	}
 
+	public void insertMessage(String str){
+		TableLayout tl = (TableLayout) findViewById(R.id.chatText);
+		TableRow tr = new TableRow(this);
+		TextView tv1 = new TextView(this);
+		tv1.setText(str);
+		tr.addView(tv1);
+		tr.setBackgroundColor(0xFF212121);
+		TableRow.LayoutParams lp = new TableRow.LayoutParams();
+		lp.setMargins(10, 0, 0, 0);
+		tr.setLayoutParams(lp);
+		tr.setPadding(2, 2, 2, 2);
+		tl.addView(tr);
+		
+		ScrollView sv = (ScrollView) findViewById(R.id.scrollChatView);
+		sv.scrollTo(0, 10000);
+	}
+	
 	public void serverConnect(){
 
         try {
